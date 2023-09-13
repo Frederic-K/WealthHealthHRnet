76b5c69c91cb952aec5d86b7022ab76443fea383
Frederic.K <frederic.kreuder@gmail.com>
2023-09-13 10:35:00 +0200

    First init

---
+++ src/features/employeeSlice.js
@@ -0,0 +1,36 @@
+// Redux
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  employees: JSON.parse(localStorage.getItem('employees')) ?? [],
+}
+
+export const employeesSlice = createSlice({
+  name: 'employees',
+  initialState,
+  reducers: {
+    addEmployee: (state, action) => {
+      state.employees = [...state.employees, action.payload]
+      localStorage.setItem('employees', JSON.stringify(state.employees))
+    },
+    deleteEmployee: (state, action) => {
+      let selected = action.payload
+      state.employees = state.employees.filter(
+        (el) => !selected.includes(el.employeeID),
+      )
+      localStorage.setItem('employees', JSON.stringify(state.employees))
+    },
+    clearEmployeeState: () => {
+      return {
+        employees: [],
+      }
+    },
+  },
+})
+// Actions to manage state
+export const { addEmployee, deleteEmployee, clearEmployeeState } =
+  employeesSlice.actions
+// Grab state
+export const employeeSelector = (state) => state.employees
+
+export default employeesSlice.reducer
---
+++ src/features/userSlice.js
@@ -0,0 +1,88 @@
+// Redux
+import { createSlice } from '@reduxjs/toolkit'
+// API
+import { GetUserData } from '../services/API/GetUserData'
+
+const initialState = {
+  id: localStorage.getItem('userID') ?? null,
+  firstName: localStorage.getItem('userFirstName') ?? null,
+  lastName: localStorage.getItem('userLastName') ?? null,
+  isFetching: false,
+  // isUpdated: false,
+  isError: false,
+  errorMessage: '',
+  successMessage: '',
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    // State management from actions
+    clearUserState: (state) => {
+      localStorage.removeItem('userID', 'userFirstName', 'userLastName')
+      localStorage.removeItem('userFirstName')
+      localStorage.removeItem('userLastName')
+      return {
+        ...state,
+        id: '',
+        firstName: '',
+        lastName: '',
+        isFetching: false,
+        // isUpdated: false,
+        isError: false,
+        errorMessage: '',
+        successMessage: '',
+      }
+    },
+    logout: (state) => {
+      localStorage.clear()
+      return {
+        ...state,
+        id: '',
+        firstName: '',
+        lastName: '',
+        isFetching: false,
+        // isUpdated: false,
+        isError: false,
+        errorMessage: '',
+        successMessage: '',
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    // State management from api feedback
+    builder
+      .addCase(GetUserData.fulfilled, (state, { payload }) => {
+        // console.log('payload', payload)
+        return {
+          ...state,
+          id: payload.userData.id,
+          firstName: payload.userData.firstName,
+          lastName: payload.userData.lastName,
+          isFetching: false,
+          successMessage: 'Login success',
+        }
+      })
+      .addCase(GetUserData.rejected, (state, { payload }) => {
+        return {
+          ...state,
+          isFetching: false,
+          isError: true,
+          errorMessage: 'Request rejected',
+        }
+      })
+      .addCase(GetUserData.pending, (state) => {
+        return {
+          ...state,
+          isFetching: true,
+        }
+      })
+  },
+})
+// Actions to manage state
+export const { clearUserState, logout } = userSlice.actions
+// Grab states
+export const userSelector = (state) => state.user
+
+export default userSlice.reducer
